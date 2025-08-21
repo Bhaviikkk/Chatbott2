889c0b1d7547d142d09ada6f62953e1f0b189f4e
Bhaviikkk <annu.joshi.nov15@gmail.com>
2025-08-21 22:30:04 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,11 @@
 module.exports = {
-  extends: ["next/core-web-vitals"],
+  extends: ["next/core-web-vitals", "@typescript-eslint/recommended"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-unused-vars": "warn",
-    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-explicit-any": "off",
     "react-hooks/exhaustive-deps": "warn",
+    "react/no-unescaped-entities": "off",
   },
 }
